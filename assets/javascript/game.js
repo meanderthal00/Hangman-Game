901bbfdf827bf9385b6a12b9af2f8538a0af9823
Meanderthal00 <meanderthal00@yahoo.com>
2017-11-06 05:19:22 -0500

    contiuing html layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var words1 = [
+    "elves",
+    "orcs",
+    "halflings",
+    "wizard",
+    "fighter",
+    "thief",
+    "beholder",
+    "dragon",
+    "goblins",
+    "dungeon master"
+
+];
+
+var el = document.getElementById("demo");
+el.innerHtml = words1;
+
+document.onkeyup = function(event) {
+    anyKey.text
+}
