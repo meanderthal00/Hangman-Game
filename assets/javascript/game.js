31569c29bb203f8c9f08795be56675ae2bf395fa
Meanderthal00 <meanderthal00@yahoo.com>
2017-11-09 07:41:59 -0500

    cleaned up code, tried new things...still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,48 @@
+// stating variables
 var words1 = ["elves","orcs","halflings","wizard","fighter","thief","beholder","dragon","goblins","dungeon master"];
-var playerGuess =
+var possibleLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","z"];
+var right = [];
+var wrong = [];
+var answer = [];
+// beginning settings
+var winTotal = 0;
+var lossTotal = 0;
+// writes above to the html
+document.getElementById("wins").innerHTML = winTotal;
+document.getElementById("losses").innerHTML =lossTotal;
 // recording user keypress
 document.onkeyup = function (event) {
     var letter = String.fromCharCode(event.keyCode).toLowerCase();
     console.log(letter);
     var letter = document.getElementById("guessed").innerHTML = letter;
-    
-}
+    }
 // selecting random word
 var word = words1[Math.floor(Math.random() * words1.length)];
 console.log(word);
 var remainingLetters = word.length;
-
-// displaying the answer array
-var answerArray = document.getElementById("demo");
+    
+// displaying the answer blanks
+var answerArray = document.getElementById("target");
 for (var i = 0; i < remainingLetters; i++) {
     answerArray.innerHTML += "_"
 };
-// the game loop starts here
-// while (remainingLetters > 0) {
+// checking users input against the answers
+if (possibleLetters.indexOf(words1) === -1) {
+    
+} else if (random.indexOf(words1) === -1) {
+   wrong.push(letter); 
+   console.log("incorrect:", wrong);
 
-// alert ("Good job! the answer was " + word + ".");
+} else {
+    console.log (letter);
+    for (var j = 0; j < random.length; j++) {
+        right[j] = letter;
+        remainingLetters --;
+        document.getElementById("target").innerHTML = answer.join("");
+
+    } 
+}
+
+
+
+
